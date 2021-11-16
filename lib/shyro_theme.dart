7e9f8464c2bb9e305791cc163fb6ac477b91f8d2
Tembeon <tembeon@gmail.com>
2021-11-16 23:33:21 +0300

    Initial commit

---
+++ lib/shyro_theme.dart
@@ -0,0 +1,13 @@
+library shyro_theme;
+
+import 'package:flutter/material.dart';
+
+class ShyroTheme {
+
+  static final ThemeData lightTheme = ThemeData();
+
+
+
+
+  static final ThemeData darkTheme = ThemeData();
+}
