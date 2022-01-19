55de2be9c5ec547ab9625335f8bb0d66aa5017c7
Lazurit <lazurit.vladislav@gmail.com>
2022-01-19 20:13:53 +0300

    example_app: rearrange Headlines and add BodyText

--- example/example_app/lib/pages/text_theme_page.dart
+++ example/example_app/lib/pages/text_theme_page.dart
@@ -2,35 +2,20 @@ import 'package:flutter/material.dart';
 
 class TextThemeTestPage extends StatelessWidget {
   const TextThemeTestPage({Key? key}) : super(key: key);
-  final String lorem = 'Lorem ipsum dolor sit amet';
+  final String lorem = 'Lorem';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        minimum: const EdgeInsets.all(12.0),
         child: SingleChildScrollView(
+          padding: const EdgeInsets.all(12.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                'H6 - $lorem',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              const Divider(),
-              Text(
-                'H5 - $lorem',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-              const Divider(),
-              Text(
-                'H4 - $lorem',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              const Divider(),
-              Text(
-                'H3 - $lorem',
-                style: Theme.of(context).textTheme.headline3,
+                'H1 - $lorem',
+                style: Theme.of(context).textTheme.headline1,
               ),
               const Divider(),
               Text(
@@ -39,10 +24,36 @@ class TextThemeTestPage extends StatelessWidget {
               ),
               const Divider(),
               Text(
-                'H1 - $lorem',
-                style: Theme.of(context).textTheme.headline1,
+                'H3 - $lorem',
+                style: Theme.of(context).textTheme.headline3,
               ),
               const Divider(),
+              Text(
+                'H4 - $lorem',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              const Divider(),
+              Text(
+                'H5 - $lorem',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              const Divider(),
+              Text(
+                'H6 - $lorem',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              const Divider(),
+              Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed d'
+                'o eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+                'Ut enim ad minim veniam, quis nostrud exercitation ullamco lab'
+                'oris nisi ut aliquip ex ea commodo consequat. Duis aute irure '
+                'dolor in reprehenderit in voluptate velit esse cillum dolore e'
+                'u fugiat nulla pariatur. Excepteur sint occaecat cupidatat non'
+                ' proident, sunt in culpa qui officia deserunt mollit anim id e'
+                'st laborum.',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
             ],
           ),
         ),
